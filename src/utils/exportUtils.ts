141bedf3252fcfe242172f2005dfd3612a04c566
vuraweg <virat52418@gmail.com>
2025-09-17 17:49:48 +0530

    Updated exportUtils.ts

--- src/utils/exportUtils.ts
+++ src/utils/exportUtils.ts
@@ -694,7 +694,7 @@ function drawCareerObjective(state: PageState, objective: string, PDF_CONFIG: an
     maxWidth: PDF_CONFIG.contentWidth,
   });
   totalHeight += objectiveHeight;
-  state.currentY += 3;
+  state.currentY += PDF_CONFIG.spacing.entrySpacing * 0.5;
   return totalHeight;
 }
 
